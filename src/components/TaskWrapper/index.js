55a0c93540028ff7ddb7732084e57e7fa2470c81
Kenny Afonso <kafonso.dev@gmail.com>
2022-07-14 19:24:43 -0100

    order tasks by status

--- src/components/TaskWrapper/index.js
+++ src/components/TaskWrapper/index.js
@@ -10,7 +10,10 @@ export default function TaskWrapper() {
       const response = await fetch("http://localhost:3333/tasks");
       const data = await response.json();
 
+      data.sort((a,b) => a.completed / b.completed); //order task by completed
+
       setTasks(data);
+      
     } catch (error) {
       console.log(error);
     }
